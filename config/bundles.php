9063113d7e99b32e6a74442e019e6c04aae9fa5a
Kevin Bond <kevinbond@gmail.com>
2025-03-10 17:00:34 -0400

    add `orm-fixtures`

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Zenstruck\Messenger\Monitor\ZenstruckMessengerMonitorBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
