fd704d5fd241b262d188a4566a15eba1d0907d0a
Kevin Bond <kevinbond@gmail.com>
2025-03-10 16:55:38 -0400

    add `zenstruck/messenger-monitor-bundle`

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Zenstruck\Messenger\Monitor\ZenstruckMessengerMonitorBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
--- src/Schedule.php
+++ src/Schedule.php
@@ -2,16 +2,40 @@
 
 namespace App;
 
+use Symfony\Component\Console\Messenger\RunCommandMessage;
+use Symfony\Component\DependencyInjection\Attribute\Target;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
+use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Schedule as SymfonySchedule;
 use Symfony\Component\Scheduler\ScheduleProviderInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 #[AsSchedule]
 final class Schedule implements ScheduleProviderInterface
 {
+    public function __construct(
+        #[Target('schedule_cache_pool')]
+        private CacheInterface $cache,
+    ) {
+    }
+
     public function getSchedule(): SymfonySchedule
     {
         return (new SymfonySchedule())
+            ->stateful($this->cache) // ensure missed tasks are executed
+            ->processOnlyLastMissedRun(true) // ensure only last missed task is run
+
+            // purge old (non-schedule) messenger monitor history daily
+            ->add(RecurringMessage::cron(
+                '#midnight',
+                new RunCommandMessage('messenger:monitor:purge --exclude-schedules'),
+            )->withJitter())
+
+            // purge old schedule messenger monitor history (keeping last 20)
+            ->add(RecurringMessage::cron(
+                '#midnight',
+                new RunCommandMessage('messenger:monitor:schedule:purge --remove-orphans --keep 20'),
+            )->withJitter())
 
             // add your own tasks here
         ;
