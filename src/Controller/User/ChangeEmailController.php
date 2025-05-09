23e43cd2e82c55e5e3e806bccc9ff1b6d941af4e
Kevin Bond <kevinbond@gmail.com>
2025-05-09 12:12:28 -0400

    Change Email (#25)

--- src/Controller/User/ChangeEmailController.php
+++ src/Controller/User/ChangeEmailController.php
@@ -3,32 +3,39 @@
 namespace App\Controller\User;
 
 use App\Entity\User;
-use App\Form\UserProfileForm;
+use App\Form\ChangeEmailForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-final class ProfileController extends AbstractController
+final class ChangeEmailController extends AbstractController
 {
-    #[Route(path: '/profile', name: 'user_profile')]
-    public function profile(
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/change-email', name: 'change_email')]
+    public function changeEmail(
         Request $request,
         EntityManagerInterface $em,
+        Security $security,
 
         #[CurrentUser]
         User $user,
-    ): Response {
-        $form = $this->createForm(UserProfileForm::class, $user);
+    ) {
+        $form = $this->createForm(ChangeEmailForm::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em->flush();
-            $this->addFlash('success', 'You\'ve successfully updated your profile.');
 
-            return $this->redirectToRoute('user_profile');
+            // since the user identifier changed, we need to re-authenticate
+            $security->login($user, 'form_login');
+
+            $this->addFlash('success', 'You have successfully changed your email.');
+
+            return $this->forward(VerificationController::class.'::sendVerification');
         }
 
         if ($form->isSubmitted()) {
@@ -37,7 +44,7 @@ final class ProfileController extends AbstractController
             $em->detach($user);
         }
 
-        return $this->render('user/profile.html.twig', [
+        return $this->render('user/change_email.html.twig', [
             'form' => $form,
         ]);
     }
